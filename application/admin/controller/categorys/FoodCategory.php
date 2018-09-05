2a6661b7a1b9f1a73b2912506330b3a2a5446a8c
85281733@qq.com <85281733@qq.com>
2018-09-05 17:44:02 +0800

    后台分类

---
+++ application/admin/controller/categorys/FoodCategory.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/5 0005
+ * Time: 上午 10:38
+ */
+
+namespace app\admin\controller\categorys;
+
+use app\common\controller\Backend;
+use app\common\model\Category as CategoryModel;
+use fast\Tree;
+/**
+ * 分类管理
+ *
+ * @icon fa fa-list
+ * @remark 用于统一管理网站的所有分类,分类可进行无限级分类
+ */
+class FoodCategory extends Backend
+{
+    public function index(){
+
+        return $this->fetch();
+    }
+}
